d3f05c0ce104aacd3c7360e7b056389054005f84
Wadson Garbes <wadson.garbes.goncalves@gmail.com>
2024-07-09 13:51:47 -0300

    Adiciona teste para garantir o uso do template home.html

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import resolve
+from django.urls import resolve, reverse
 from lists.views import HomePageView
 
 class HomePageTest(TestCase):
@@ -7,3 +7,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func.__name__, HomePageView.as_view().__name__)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get(reverse('Home'))
+        self.assertTemplateUsed(response, 'lists/home.html')
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn('<title>To-Do Lists</title>', html)
+        self.assertTrue(html.strip().endswith("</html>"))
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import TemplateView
 
-class HomePageView(View):
-    def get(self):
-        return "Hello, World"
+class HomePageView(TemplateView):
+    template_name = 'lists/home.html'
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
